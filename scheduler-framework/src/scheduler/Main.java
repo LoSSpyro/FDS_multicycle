c90ad04aae20398045d2a73e67abd2a3a773d7ec
Simon Vetter <simon_michael.vetter@stud.tu-darmstadt.de>
2018-11-17 17:01:29 +0100

    Test for ASAP_Fixed

--- scheduler-framework/src/scheduler/Main.java
+++ scheduler-framework/src/scheduler/Main.java
@@ -3,6 +3,47 @@ package scheduler;
 public class Main {
 
 	public static void main(String[] args) {
+		testASAPFixed(args);
+	}
+	
+	public static void testASAPFixed(String[] args) {
+		RC rc = null;
+		if (args.length>1){
+			System.out.println("Reading resource constraints from "+args[1]+"\n");
+			rc = new RC();
+			rc.parse(args[1]);
+		}
+		
+		Dot_reader dr = new Dot_reader(false);
+		if (args.length < 1) {
+			System.err.printf("Usage: scheduler dotfile%n");
+			System.exit(-1);
+		}else {
+			System.out.println("Scheduling "+args[0]);
+			System.out.println();
+		}
+		
+		Graph g = dr.parse(args[0]);
+		System.out.printf("%s%n", g.diagnose());
+		
+		
+		
+		ASAP asap = new ASAP();
+		Schedule sched = asap.schedule(g);
+		System.out.printf("%nASAP%n%s%n", sched.diagnose());
+		System.out.printf("cost = %s%n", sched.cost());
+		
+		sched.draw("schedules/ASAP_" + args[0].substring(args[0].lastIndexOf("/")+1));
+		
+		ASAP_Fixed asap_fixed = new ASAP_Fixed();
+		sched = asap_fixed.schedule(g, new Schedule());
+		System.out.printf("%nASAP_Fixed%n%s%n", sched.diagnose());
+		System.out.printf("cost = %s%n", sched.cost());
+		
+		sched.draw("schedules/ASAP_Fixed_" + args[0].substring(args[0].lastIndexOf("/")+1));
+	}
+	
+	public static void defaultMain(String[] args) {
 		RC rc = null;
 		if (args.length>1){
 			System.out.println("Reading resource constraints from "+args[1]+"\n");
