922c027b88c5b197fc2283f2e8605767fb0bb664
Simon Vetter <simon_michael.vetter@stud.tu-darmstadt.de>
2018-11-19 13:39:23 +0100

    Small codestyle improvements

--- scheduler-framework/src/scheduler/testing/Test_ASAP_Fixed.java
+++ scheduler-framework/src/scheduler/testing/Test_ASAP_Fixed.java
@@ -18,6 +18,7 @@ class Test_ASAP_Fixed {
 	private static String resourceFile = "resources/heterogenous_16res";
 	private static String graphFile = "graphs/testCyclic.dot";
 	private static Schedule reference;
+	private static Schedule partialSchedule;
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -29,40 +30,23 @@ class Test_ASAP_Fixed {
 		System.out.printf("%s%n", graph.diagnose());
 	}
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
 	@BeforeEach
 	void setUp() throws Exception {
 		reference = new ASAP().schedule(graph);
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
+		partialSchedule = new Schedule();
 	}
 
 	@Test
 	void testASAPNoFixed() {
-		Schedule partialSchedule = new Schedule();
 		Schedule asap_fixed = new ASAP_Fixed().schedule(graph, partialSchedule);
 		
 		assertEquals(asap_fixed.length(), reference.length(),
 				"Schedule should be the same length as the reference ASAP schedule");
-		for (Node node : asap_fixed.getNodes().keySet()) {
-			int startTime = asap_fixed.getNodes().get(node).lbound;
-			int endTime = asap_fixed.getNodes().get(node).ubound;
-			assertEquals(endTime - startTime, node.getDelay() - 1, "Incorrect interval length");
-			for (Node successor : node.allSuccessors().keySet()) {
-				int succStartTime = asap_fixed.getNodes().get(successor).lbound;
-				assertTrue(succStartTime > endTime, "Data dependency violated");
-			}
-		}
+		checkIntervalsAndDependencies(asap_fixed);
 	}
 	
 	@Test
 	void testASAPOneFixed() {
-		Schedule partialSchedule = new Schedule();
 		Iterator<Node> iter = graph.iterator();
 		Node n1 = null;
 		while (iter.hasNext()) {
@@ -77,20 +61,11 @@ class Test_ASAP_Fixed {
 		assertTrue(asap_fixed.length() >= reference.length(),
 				"Schedule cannot be shorter than the reference ASAP schedule");
 		assertEquals(asap_fixed.length().intValue(), 12, "Optimal schedule under the given constraints wasn't found");
-		for (Node node : asap_fixed.getNodes().keySet()) {
-			int startTime = asap_fixed.getNodes().get(node).lbound;
-			int endTime = asap_fixed.getNodes().get(node).ubound;
-			assertEquals(endTime - startTime, node.getDelay() - 1, "Incorrect interval length");
-			for (Node successor : node.allSuccessors().keySet()) {
-				int succStartTime = asap_fixed.getNodes().get(successor).lbound;
-				assertTrue(succStartTime > endTime, "Data dependency violated");
-			}
-		}
+		checkIntervalsAndDependencies(asap_fixed);
 	}
 	
 	@Test
 	void testASAPTwoFixed() {
-		Schedule partialSchedule = new Schedule();
 		Iterator<Node> iter = graph.iterator();
 		Node n1 = null, n2 = null;
 		while ((n1 == null || n2 == null) && iter.hasNext()) {
@@ -106,6 +81,18 @@ class Test_ASAP_Fixed {
 		
 		assertTrue(asap_fixed == null,
 				"Schedule was delivered where no legal schedule is possible");
+	}
+	
+	void checkIntervalsAndDependencies(Schedule schedule) {
+		for (Node node : schedule.getNodes().keySet()) {
+			int startTime = schedule.getNodes().get(node).lbound;
+			int endTime = schedule.getNodes().get(node).ubound;
+			assertEquals(endTime - startTime, node.getDelay() - 1, "Incorrect interval length");
+			for (Node successor : node.allSuccessors().keySet()) {
+				int succStartTime = schedule.getNodes().get(successor).lbound;
+				assertTrue(succStartTime > endTime, "Data dependency violated");
+			}
+		}
 	}
 
 }
